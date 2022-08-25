2cc9d266cdd5715564ad76d688e55a720807a135
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-08-25 21:32:35 +0530

    Created the interface

--- game.cpp
+++ game.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
     cout << "--- Guess Me : The Number Challenge ----" << endl;
+    
+    // Instructions 
+    cout << "Instructions: \nA random number between 1 and 100 will be generated.\nYou have to guess the number. Every time you guess the wrong number it will indicated you weather the generated number is higher or lower.\nOn the baises of no. of guesses took your score will be decided." << endl ;
     getch();
+    return 0;
 }
