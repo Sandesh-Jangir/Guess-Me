e12d50e0cd1edc7be1e78e1f9c80f89148ebc946
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-08-26 18:12:29 +0530

    created the mainloop

--- game.cpp
+++ game.cpp
@@ -8,6 +8,17 @@ int main()
     
     // Instructions 
     cout << "Instructions: \nA random number between 1 and 100 will be generated.\nYou have to guess the number. Every time you guess the wrong number it will indicated you weather the generated number is higher or lower.\nOn the baises of no. of guesses took your score will be decided." << endl ;
+
+    int guess;
+    bool guessed = false;
+
+    // Mainloop
+    while (guessed == false)
+    {
+        cout << "Guess: ";
+        cin >> guess;
+    }
+    
     getch();
     return 0;
 }
