391e39fb7fa0a1b91f94f12bd37e43143a0f479a
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-08-27 15:12:00 +0530

    Refurbished The Interface

--- game.cpp
+++ game.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main()
 {
-    cout << "--- Guess Me : The Number Challenge ----" << endl;
+    cout << "---- Guess Me : The Number Challenge ----" << endl;
     
     // Instructions 
-    cout << "Instructions: \nA random number between 1 and 100 will be generated.\nYou have to guess the number. Every time you guess the wrong number it will indicated you weather the generated number is higher or lower.\nOn the baises of no. of guesses took your score will be decided." << endl ;
+    cout << "Instructions:\n\nA random number between 1 and 100 will be generated.\nYou have to guess the number. \nEvery time you guess the wrong number it will indicated you weather the generated number is higher or lower.\nOn the baises of no. of guesses took your score will be decided.\n" << endl ;
 
     int guess;
     bool guessed = false;
@@ -17,9 +17,10 @@ int main()
     // Generating the number 
     srand(time(0));
     int number = 1 + (rand() % 100);
+    cout << "Number Generated..." << endl;
 
     // cout << number; <- for debugging purposes only
-    
+
     // Mainloop
     int score_n = 0;
     while (guessed == false)
@@ -28,7 +29,7 @@ int main()
         cout << "Guess: ";
         cin >> guess;
         if (number == guess){
-            cout << "Congratulations !! Great Guess" << endl;
+            cout << "\nCongratulations !! Number Guessed Successfully..." << endl;
             cout <<"Score : "<< 100-score_n;
             guessed = true;
         }else if (number < guess){
